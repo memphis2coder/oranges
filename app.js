e78459227c2bb87542624ad0989690d8dbc7658f
Antonio <Memphis2Coder@gmail.com>
2019-09-07 18:45:15 -0500

    changed the navigation

---
+++ app.js
@@ -0,0 +1,18 @@
+// open overlay for navigation menu
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+// change color of navbar after window scroll
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+// smooth scroll
